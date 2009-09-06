7525795ec3d661016a3d43e1d3d79f1612b8d062
jjhatf02 <jjhatf02@5cf97afc-9a85-11de-a465-5fc2cdd6756b>
2009-09-06 02:03:42 +0000

    DyKnow reader library that is able to open a DyKnow file and read in some simple information about user submitted panels.

--- client/DPX/QuickReader/DyKnowPage.cs
+++ client/DPX/QuickReader/DyKnowPage.cs
@@ -2,10 +2,134 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
 
 namespace QuickReader
 {
-    class DyKnowPage
+    public class DyKnowPage
     {
+        private int pageNumber;
+        private String userName;
+        private String fullName;
+        private int strokes;
+        private String finished;
+
+        //Constructor accepts the XML sub tree
+        public DyKnowPage(XmlReader xmlFile, int pageNum)
+        {
+            //Set some default values if the xml parsing doesn't workout
+            pageNumber = pageNum;
+            userName = "";
+            fullName = "";
+            strokes = 0;
+            finished = "";
+
+            List<String> myStrokes = new List<string>();
+
+            while (xmlFile.Read())
+            {
+                //Used to identify the user name and full name
+                if (xmlFile.Name.ToString() == "PAGE")
+                {
+                    while (xmlFile.MoveToNextAttribute())
+                    {
+                        //User Name
+                        if (xmlFile.Name.ToString() == "ONER")
+                        {
+                            userName = xmlFile.Value.ToString();
+                        }
+                        //Full Name
+                        else if (xmlFile.Name.ToString() == "ONERN")
+                        {
+                            fullName = xmlFile.Value.ToString();
+                        }
+                    }
+
+                }
+                //Used to count the pen strokes on the page
+                else if (xmlFile.Name.ToString() == "PEN")
+                {
+                    //Only count user pen strokes
+                    if (xmlFile.GetAttribute("UT").ToString() == "0")
+                    {
+                        //Stroke taken
+                        myStrokes.Add(xmlFile.GetAttribute("UID").ToString());
+                    }
+                }
+                else if (xmlFile.Name.ToString() == "DEOB")
+                {
+                    //Stroke removed
+                    if (xmlFile.GetAttribute("UT") == "0")
+                    {
+                        deleteStrokes(myStrokes, xmlFile.ReadSubtree());
+                    }
+                }
+            }
+            //Net number of strokes on page
+            strokes = myStrokes.Count;
+        }
+
+        //Used to delete strokes from the list of strokes
+        private void deleteStrokes(List<string> myStrokes, XmlReader xmlFile)
+        {
+            while (xmlFile.Read())
+            {
+                if (xmlFile.Name.ToString() == "EDDE")
+                {
+                    myStrokes.Remove(xmlFile.GetAttribute("OBJID").ToString());
+                }
+            }
+        }
+
+        public void setFinished(String st)
+        {
+            finished = st;
+        }
+
+        public int getPageNumber()
+        {
+            return pageNumber;
+        }
+
+        public string getUserName()
+        {
+            return userName;
+        }
+
+        public string getFullName()
+        {
+            return fullName;
+        }
+
+        public int getStrokeCount()
+        {
+            return strokes;
+        }
+
+        public String getFinished()
+        {
+            return finished;
+        }
+
+        //Used to get the information form the page for display purposes.
+        public String[] getData()
+        {
+
+            string[] data = { pageNumber.ToString(), userName, fullName, strokes.ToString(), finished };
+            return data;
+        }
+
+        public object[] getRowData()
+        {
+            object[] data = { pageNumber, userName, fullName, strokes, finished };
+            return data;
+        }
+
+        //Used for testing
+        public override string ToString()
+        {
+            //return base.ToString();
+            return pageNumber.ToString() + ", " + userName + ", " + fullName + ", " + strokes.ToString() + ", " + finished;
+        }
     }
 }
--- client/DPX/QuickReader/DyKnowReader.cs
+++ client/DPX/QuickReader/DyKnowReader.cs
@@ -2,10 +2,143 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Xml;
+using System.IO.Compression;
+using System.IO;
 
 namespace QuickReader
 {
-    class DyKnowReader
+    public class DyKnowReader
     {
+        private FileStream inputFile;
+        private GZipStream gzipFile;
+        private XmlTextReader xmlFile;
+        private String fileName;
+        private List<DyKnowPage> dyKnowPages;
+
+        private double meanStrokes;
+        private double stdDevStrokes;
+
+        public DyKnowReader(string name)
+        {
+            //The name of the DyKnow file used
+            fileName = name;
+            //Open the file
+            inputFile = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+            //Uncompress the file
+            gzipFile = new GZipStream(inputFile, CompressionMode.Decompress, true);
+            //Read the uncompressed file as XML
+            xmlFile = new XmlTextReader(gzipFile);
+            //The collection of pages
+            dyKnowPages = new List<DyKnowPage>();
+            //Some more default values
+            meanStrokes = 0;
+            stdDevStrokes = 0;
+
+            int myRow = 1;
+            //Loop through the XML information
+            while (xmlFile.Read())
+            {
+                if (xmlFile.NodeType == XmlNodeType.Element)
+                {
+                    //We only care about the PAGE nodes
+                    if (xmlFile.Name.ToString() == "PAGE")
+                    {
+
+                        //Process the PAGE sub-tree and store the inforation
+                        DyKnowPage panel = new DyKnowPage(xmlFile.ReadSubtree(), myRow++);
+                        
+                        //Add the page information to the list of pages
+                        dyKnowPages.Add(panel);
+                    }
+                }
+            }
+
+            meanStrokes = CalcMeanStrokes();
+            stdDevStrokes = CalcStdDevStrokes(meanStrokes);
+            fillInFinished();
+        }
+
+        private void fillInFinished()
+        {
+            for (int i = 0; i < NumOfPages(); i++)
+            {
+                if (dyKnowPages[i].getStrokeCount() == 0)
+                {
+                    dyKnowPages[i].setFinished("No");
+                }
+                else if (dyKnowPages[i].getStrokeCount() < (meanStrokes - (2 * stdDevStrokes)))
+                {
+                    dyKnowPages[i].setFinished("Maybe");
+                }
+                else
+                {
+                    dyKnowPages[i].setFinished("Yes");
+                }
+            }
+        }
+
+        private double CalcMeanStrokes()
+        {
+            long total = 0;
+            for (int i = 0; i < NumOfPages(); i++)
+            {
+                total += dyKnowPages[i].getStrokeCount();
+            }
+            return (double)total / (double)NumOfPages();
+        }
+
+        private double CalcStdDevStrokes(double mean)
+        {
+            double total = 0;
+            for (int i = 0; i < NumOfPages(); i++)
+            {
+                total += Math.Pow((dyKnowPages[i].getStrokeCount() - mean), 2);
+            }
+            return Math.Sqrt((double)total / (double)(NumOfPages() - 1));
+        }
+
+        public double GetMeanStrokes()
+        {
+            return meanStrokes;
+        }
+
+        public double GetStdDevStrokes()
+        {
+            return stdDevStrokes;
+        }
+
+        public void Close()
+        {
+            inputFile.Close();
+            gzipFile.Close();
+            xmlFile.Close();
+        }
+
+        public DyKnowPage getDyKnowPage(int i)
+        {
+            return dyKnowPages[i];
+        }
+
+        //Used in testing
+        public String GetPageString(int i)
+        {
+            return dyKnowPages[i].ToString();
+        }
+
+
+        public object[] GetRowData(int i)
+        {
+            return dyKnowPages[i].getRowData();
+        }
+
+
+        public int NumOfPages()
+        {
+            return dyKnowPages.Count;
+        }
+
+
+
     }
 }
