a5b5c7908759275cc7991d4e9f9288d23ce81ce1
jjhatf02 <jjhatf02@5cf97afc-9a85-11de-a465-5fc2cdd6756b>
2009-09-06 02:52:54 +0000

    A simple command line program that demonstrates reading in a DyKnow file and extracting user information from it.

--- client/DPX/UnitTests/Program.cs
+++ client/DPX/UnitTests/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using QuickReader;
 
 namespace UnitTests
 {
@@ -9,6 +10,19 @@ namespace UnitTests
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("DyKnow Panel eXtractor Unit Tests");
+            Console.Write("File Name: ");
+            String file = Console.ReadLine();
+            DyKnowReader dr = new DyKnowReader(file);
+
+            for (int i = 0; i < dr.NumOfPages(); i++)
+            {
+                Console.WriteLine(dr.GetPageString(i));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadLine();
         }
     }
 }
