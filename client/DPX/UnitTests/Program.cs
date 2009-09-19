2232fcd77b45b7d9a598d6c4e7aa2fb93ea1873e
jjhatf02 <jjhatf02@5cf97afc-9a85-11de-a465-5fc2cdd6756b>
2009-09-19 17:48:07 +0000

    Began the implementation of a GUI using WPF for the DPX tool.

--- client/DPX/UnitTests/Program.cs
+++ client/DPX/UnitTests/Program.cs
@@ -19,12 +19,9 @@ namespace UnitTests
             String dyknowfiled = "C:\\Users\\Jared\\Desktop\\samplefile.dyz";
 
             Database db = new Database(dbfile);
-
             DyKnowReader dr = new DyKnowReader(dyknowfiled);
 
-            //Exceptions e = new Exceptions(5, 180, 1, "This is a test");
-            //db.addException(e);
-
+            
             List<Classdate> cd = db.getClassdates();
             for (int i = 0; i < cd.Count; i++)
             {
@@ -36,25 +33,7 @@ namespace UnitTests
             {
                 Console.WriteLine(r[i].ToString());
             }
-            //db.addStudent(new Student(dr.getDyKnowPage(0)));
-            //db.addSection("101-01");
-            //Console.WriteLine(db.getSectionName(1));
-            /*
-            int fileId = db.addFile(dr, DateTime.Now);
-            for (int i = 0; i < dr.NumOfPages(); i++)
-            {
-                if (!db.isStudentUsername(dr.getDyKnowPage(i).UserName))
-                {
-                    Console.WriteLine("Student isn't in db: " + dr.getDyKnowPage(i).UserName);
-                    db.addStudent(new Student(dr.getDyKnowPage(i)));
-                }
-                db.addPanel(fileId, dr.getDyKnowPage(i));
-                //Console.Write(".");
-            }
             
-            Console.WriteLine();
-            Console.WriteLine("Inserted File ID: " + fileId.ToString());
-            */
             Console.WriteLine();
             Console.WriteLine("Press any key to exit...");
             Console.ReadLine();
@@ -64,96 +43,6 @@ namespace UnitTests
         {
             PanelSorter ps = new PanelSorter(inputfile, outputfile);
             ps.processSort();
-        }
-
-        public static void readdyknowfile()
-        {
-            //Get the DyKnow file ready
-            Console.Write("File Name: ");
-            String file = Console.ReadLine();
-            DyKnowReader dr = new DyKnowReader(file);
-
-            for (int i = 0; i < dr.NumOfPages(); i++)
-            {
-                Console.WriteLine(dr.GetPageString(i));
-            }
-
-            Console.WriteLine();
-            Console.WriteLine(dr.ToString());
-            Console.WriteLine("Minimum: " + dr.MinStrokeCount);
-            Console.WriteLine("Maximum: " + dr.MaxStrokeCount);
-        }
-
-        public static void databaseTest()
-        {
-
-            //Get the database ready
-            Console.Write("Database File: ");
-            String db = Console.ReadLine();
-            aConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + db + ";Persist Security Info=False");
-
-            //Get the DyKnow file ready
-            Console.Write("File Name: ");
-            String file = Console.ReadLine();
-            DyKnowReader dr = new DyKnowReader(file);
-
-            aConnection.Open();
-            for (int i = 0; i < dr.NumOfPages(); i++)
-            {
-                Console.WriteLine(dr.GetPageString(i));
-                InsertNewRecord(file, dr.getDyKnowPage(i));
-            }
-            aConnection.Close();
-        }
-
-
-        public static bool InsertNewRecord(String file, DyKnowPage page)
-        {
-            OleDbCommand cmdInsert = new OleDbCommand();
-            string query = "INSERT INTO panels(file, panelnumber, username, fullname, strokes, complete) VALUES(@parm1, @parm2, @parm3, @parm4, @parm5, @parm6)";
-            int status;
-
-            cmdInsert.Parameters.Clear();
-            try
-            {
-                //Give it the query
-                cmdInsert.CommandText = query;
-                //Type of query
-                cmdInsert.CommandType = System.Data.CommandType.Text;
-                //Add parameters to the query
-                cmdInsert.Parameters.AddWithValue("@parm1", file);
-                cmdInsert.Parameters.AddWithValue("@parm2", page.PageNumber);
-                cmdInsert.Parameters.AddWithValue("@parm3", page.UserName);
-                cmdInsert.Parameters.AddWithValue("@parm4", page.FullName);
-                cmdInsert.Parameters.AddWithValue("@parm5", page.NetStrokeCount);
-                cmdInsert.Parameters.AddWithValue("@parm6", page.Finished);
-                
-                //Specify connection
-                cmdInsert.Connection = aConnection;
-
-                // 0 = failed, 1 = success
-                status = cmdInsert.ExecuteNonQuery();
-                if (!(status == 0))
-                {
-                    //It Failed
-                    return false;
-                }
-                else
-                {
-                    //It Worked!
-                    return true;
-                }
-            }
-            catch (Exception ex)
-            {
-                //Display the error
-                Console.WriteLine(ex.Message, "Error");
-            }
-            finally
-            {
-                //All done
-            }
-            return true;
         }
 
 
