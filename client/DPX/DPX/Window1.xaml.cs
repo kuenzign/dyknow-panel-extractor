e0459e0033d2aa3385ce2b4c4d68b70fcab21597
jjhatf02 <jjhatf02@5cf97afc-9a85-11de-a465-5fc2cdd6756b>
2010-01-09 19:04:25 +0000

    Fixed issue 4. Added the ability to create a new database from the file new menu.

--- client/DPX/DPX/Window1.xaml.cs
+++ client/DPX/DPX/Window1.xaml.cs
@@ -77,6 +77,7 @@ namespace DPX
                         FileMenuOpen.IsEnabled = false;
                         FileMenuClose.IsEnabled = true;
                         FileMenuRoster.IsEnabled = true;
+                        FileMenuNew.IsEnabled = false;
                     }
             }
         }
@@ -92,6 +93,7 @@ namespace DPX
             FileMenuOpen.IsEnabled = true;
             FileMenuClose.IsEnabled = false;
             FileMenuRoster.IsEnabled = false;
+            FileMenuNew.IsEnabled = true;
         }
 
         /// <summary>
@@ -106,6 +108,49 @@ namespace DPX
             {
                 ri.ShowDialog();
             }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NewFile(object sender, RoutedEventArgs e)
+        {
+            // Location of the empty database that is included as part of the installer
+            String locationOfNewDatabase = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase) + "\\NewDPXDatabase.accdb";
+            
+            // Remove file:\ from the beginning of the string
+            locationOfNewDatabase = locationOfNewDatabase.Substring(6);
+
+            // Configure save file dialog box
+            Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+            dlg.DefaultExt = ".seat"; // Default file extension
+            dlg.Filter = "Database (.accdb)|*.accdb"; // Filter files by extension
+
+            // Show open file dialog box
+            Nullable<bool> result = dlg.ShowDialog();
+
+            // Process open file dialog box results
+            if (result == true)
+            {
+                if (System.IO.File.Exists(locationOfNewDatabase))
+                {
+                    System.IO.File.Copy(locationOfNewDatabase, dlg.FileName);
+
+                    // Open document
+                    this.c.OpenDatabaseFile(dlg.FileName);
+                    FileMenuOpen.IsEnabled = false;
+                    FileMenuClose.IsEnabled = true;
+                    FileMenuRoster.IsEnabled = true;
+                    FileMenuNew.IsEnabled = false;
+                }
+                else
+                {
+                    MessageBox.Show("File not found: " + locationOfNewDatabase, "Error: file not found");
+                }
+            }
+            
         }
     }
 }
