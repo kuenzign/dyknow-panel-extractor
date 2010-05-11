31dceb29ea32567d33e7c0198be05173616c295c
jjhatf02 <jjhatf02@5cf97afc-9a85-11de-a465-5fc2cdd6756b>
2010-05-11 20:07:34 +0000

    Added a save "Report Button" to the generate report tab that saves the report that is located in the RichTextBox to a txt file. Also, when a report is generated it clears what is currently in the RichTextBox.

--- client/DPX/DPXManager/GenerateReport.xaml.cs
+++ client/DPX/DPXManager/GenerateReport.xaml.cs
@@ -6,6 +6,7 @@ namespace DPXManager
 {
     using System;
     using System.Collections.Generic;
+    using System.IO;
     using System.Linq;
     using System.Text;
     using System.Windows;
@@ -47,6 +48,10 @@ namespace DPXManager
         {
             if (this.c.IsDatabaseOpen())
             {
+                // Erase everything that is currently in the rich text box
+                this.richTextBoxReport.Document.Blocks.Clear();
+
+                // Generate the report and fill it in the rich text box
                 List<Classdate> selectedDates = new List<Classdate>();
                 for (int i = 0; i < listBoxDates.SelectedItems.Count; i++)
                 {
@@ -60,6 +65,41 @@ namespace DPXManager
             else
             {
                 MessageBox.Show("Database is not open.", "Error");
+            }
+        }
+
+        /// <summary>
+        /// Buttons the save report.
+        /// </summary>
+        /// <param name="sender">The sender.</param>
+        /// <param name="e">The <see cref="System.Windows.RoutedEventArgs"/> instance containing the event data.</param>
+        private void ButtonSaveReport(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Configure save file dialog box
+                Microsoft.Win32.SaveFileDialog dlg = new Microsoft.Win32.SaveFileDialog();
+                dlg.DefaultExt = ".txt"; // Default file extension
+                dlg.Filter = "Text documents (*.txt)|*.txt"; // Filter files by extension
+
+                // Show open file dialog box
+                Nullable<bool> result = dlg.ShowDialog();
+
+                // Process open file dialog box results
+                if (result == true)
+                {
+                    // Save the document
+                    TextRange range;
+                    FileStream fileStream;
+                    range = new TextRange(this.richTextBoxReport.Document.ContentStart, this.richTextBoxReport.Document.ContentEnd);
+                    fileStream = new FileStream(dlg.FileName, FileMode.Create);
+                    range.Save(fileStream, DataFormats.Text);
+                    fileStream.Close();
+                }
+            }
+            catch (Exception err)
+            {
+                MessageBox.Show(err.Message);
             }
         }
     }
