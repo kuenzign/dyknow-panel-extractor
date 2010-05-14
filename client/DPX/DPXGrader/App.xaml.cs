e02f3256b69c0b80ad056772f6ffdf9503ab64f5
jjhatf02 <jjhatf02@5cf97afc-9a85-11de-a465-5fc2cdd6756b>
2010-05-14 19:37:52 +0000

    Added a new project for DPXGrader that will implement a new application for processing DyKnow files for the purpose of grading.

---
+++ client/DPX/DPXGrader/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿// <copyright file="App.xaml.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>The main application for DPX.</summary>
+namespace DPXGrader
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Linq;
+    using System.Windows;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ client/DPX/DPXGrader/DPXGraderWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="DPXGraderWindow.xaml.cs" company="University of Louisville Speed School of Engineering">
+// GNU General Public License v3
+// </copyright>
+// <summary>The window for DPXGrader.</summary>
+namespace DPXGrader
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Data;
+    using System.Windows.Documents;
+    using System.Windows.Input;
+    using System.Windows.Media;
+    using System.Windows.Media.Imaging;
+    using System.Windows.Navigation;
+    using System.Windows.Shapes;
+
+    /// <summary>
+    /// Interaction logic for DPXGraderWindow.xaml
+    /// </summary>
+    public partial class DPXGraderWindow : Window
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DPXGraderWindow"/> class.
+        /// </summary>
+        public DPXGraderWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
