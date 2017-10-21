181773ad1e2c63cff0b5c1939d56306fd37a22a9
Nathan Kuenzig <kuenzign@gmail.com>
2017-10-21 01:31:37 -0400

    Started upgrade of DPXManager

--- DPX/DPXManager/App.xaml.cs
+++ DPX/DPXManager/App.xaml.cs
@@ -1,13 +1,12 @@
 ﻿// <copyright file="App.xaml.cs" company="University of Louisville Speed School of Engineering">
 // GNU General Public License v3
 // </copyright>
-// <summary>The main application for DPX.</summary>
+using System.Windows;
+
 namespace DPXManager
 {
-    using System.Windows;
-
     /// <summary>
-    /// The main application for DPX.
+    /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
